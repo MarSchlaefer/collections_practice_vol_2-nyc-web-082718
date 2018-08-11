986cc0029fb6e44e950e204a82ff87ff30984ff4
Mariel Schlaefer <mschlaefer00@gmail.com>
2018-08-11 19:38:27 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,4 +1,11 @@
 # your code goes here
-def begins_with_r
-  
+def begins_with_r(tools)
+  tools.sort do |a,b|
+    
 end
+
+def contain_a(array)
+  array.collect do |element|
+    element.include? ('a')
+  end
+end 
